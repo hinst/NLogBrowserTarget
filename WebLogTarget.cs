33e3cb29b86c46433671470bc27c43a659b3c5e6
hinst <hinstance@yandex.ru>
2015-11-27 13:38:19 +0300

    web server in progress

--- WebLogTarget.cs
+++ WebLogTarget.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using NLog;
 using NLog.Config;
@@ -13,11 +14,34 @@ namespace NLogBrowserTarget
 	
 		[DefaultParameter]
 		public int port { get; set; }
+		const int defaultPort = 16109;
 		readonly Queue<LogEventInfo> logEventQueue = new Queue<LogEventInfo>();
+		HttpListener listener;
+		
+		public WebLogTarget()
+		{
+			port = defaultPort;
+		}
 		
 		protected override void Write(LogEventInfo logEvent)
 		{
+			logEventQueue.Enqueue(logEvent);
+			if (listener == null)
+			{
+				listener = new HttpListener();
+				listener.Prefixes.Add("http://127.0.0.1:" + port + "/");
+				listener.Start();
+				listener.BeginGetContext(processRequest, null);
+			}
+		}
+		
+		void processRequest(IAsyncResult data)
+		{
+			var context = listener.EndGetContext(data);
+			var request = context.Request;
+			var response = context.Response;
 		}
 	
 	}
+	
 }
