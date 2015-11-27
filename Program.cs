9b44642be17c3250e1d64b7b8d02d9696b74a428
hinst <hinstance@yandex.ru>
2015-11-27 13:21:36 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+namespace NLogBrowserTarget
+{
+
+	class Program
+	{
+	
+		Logger log = LogManager.GetCurrentClassLogger();
+	
+		public static void Main(string[] args)
+		{
+			var program = new Program();
+			program.run();
+		}
+		
+		void run()
+		{
+			
+		}
+		
+	}
+	
+}
---
+++ WebLogTarget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace NLogBrowserTarget
+{
+
+	[Target("WebBrowser")]
+	public class WebLogTarget : Target
+	{
+	
+		[DefaultParameter]
+		public int port { get; set; }
+		readonly Queue<LogEventInfo> logEventQueue = new Queue<LogEventInfo>();
+		
+		protected override void Write(LogEventInfo logEvent)
+		{
+		}
+	
+	}
+}
