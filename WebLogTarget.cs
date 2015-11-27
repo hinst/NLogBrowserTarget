e285b9209934802ab9b4fb7cea14d44c25177e03
hinst <hinstance@yandex.ru>
2015-11-27 14:41:31 +0300

    log page now works

--- WebLogTarget.cs
+++ WebLogTarget.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Text;
+using System.IO;
 using System.Net;
 using System.Collections.Generic;
+using System.Reflection;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
@@ -17,14 +20,34 @@ namespace NLogBrowserTarget
 		const int defaultPort = 16109;
 		readonly Queue<LogEventInfo> logEventQueue = new Queue<LogEventInfo>();
 		HttpListener listener;
+		string logPage;
 		
 		public WebLogTarget()
 		{
 			port = defaultPort;
 			listener = new HttpListener();
-			listener.Prefixes.Add("http://127.0.0.1:" + port + "/");
+			listener.Prefixes.Add("http://127.0.0.1:" + port + "/page/");
+			listener.Prefixes.Add("http://127.0.0.1:" + port + "/update/");
 			listener.Start();
 			listener.BeginGetContext(processRequest, null);
+			prepareLogPage();
+		}
+		
+		string readTextResource(string name)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var resourceName = assembly.GetName().Name + "." + name;
+			Console.WriteLine("!!!"+resourceName+"!!!");
+			var stream = assembly.GetManifestResourceStream(resourceName);
+			var streamReader = new StreamReader(stream, Encoding.UTF8);
+			var text = streamReader.ReadToEnd();
+			return text;
+		}
+		
+		void prepareLogPage()
+		{
+			logPage = readTextResource("logPage.html");
+			logPage = logPage.Replace("$bootstrap-css$", readTextResource("bootstrap.min.css"));
 		}
 		
 		protected override void Write(LogEventInfo logEvent)
@@ -36,15 +59,24 @@ namespace NLogBrowserTarget
 		{
 			var context = listener.EndGetContext(data);
 			var request = context.Request;
+			var localPath = request.Url.LocalPath;
+			var responseText = "";
+			if (localPath == "/page/")
+			{
+				responseText = getPage();
+			}
 			var response = context.Response;
+			var responseData = Encoding.UTF8.GetBytes(responseText);
+			response.OutputStream.Write(responseData, 0, responseData.Length);
+			response.OutputStream.Close();
+			listener.BeginGetContext(processRequest, null);
 		}
 		
-		~WebLogTarget()
+		string getPage()
 		{
-			listener.Close();
-			listener = null;
+			return logPage;
 		}
-	
+		
 	}
 	
 }
